e7ab7b72287fee3fe271c7d344adc2054a6a0248
vnfernandesz <vinnicyusfernandes09@gmail.com>
2026-08-28 07:59:08 -0300

    Add scrollview component with sample text

---
+++ scrollview.jsx
@@ -0,0 +1,42 @@
+import {Text,ScrollView,SafeAreaView } from 'react-native';
+
+
+export default function App() {
+  return (
+    <SafeAreaView style={{flex:1,backgroundColor:'red'}}>
+    <ScrollView style={{flex:1,backgroundColor:'blue'}}>
+      <Text style={{margin: 50}}>
+      um texto demonstrativo
+      </Text>
+
+         <Text style={{margin: 50}}>
+      um texto demonstrativo
+      </Text>
+
+         <Text style={{margin: 50}}>
+      um texto demonstrativo
+      </Text>
+
+         <Text style={{margin: 50}}>
+      um texto demonstrativo
+      </Text>
+
+         <Text style={{margin: 50}}>
+      um texto demonstrativo
+      </Text>
+
+         <Text style={{margin: 50}}>
+      um texto demonstrativo
+      </Text>
+
+         <Text style={{margin: 50}}>
+      um texto demonstrativo
+      </Text>
+
+    </ScrollView>
+        </SafeAreaView>
+
+  );
+
+}
+
