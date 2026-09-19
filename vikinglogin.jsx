39cdce3602607d5b518ad09d466bf183447d5eaa
vnfernandesz <vinnicyusfernandes09@gmail.com>
2026-09-18 21:48:05 -0300

    stylesheet login

--- vikinglogin.jsx
+++ vikinglogin.jsx
@@ -1,54 +1,86 @@
-import {TextInput,Button,Image,Pressable,StyleSheet, Text, View } from 'react-native';
+import {TextInput, Button, Image,Pressable,StyleSheet, Text, View
+} from 'react-native';
 
 export default function App() {
   return (
 
-  <View>
-  <View style={{flex:64,alignItems:'center'}}>
+    <View style={styles.container}>
+
+  <View style={styles.imagemContainer}>
+
   <Image
-source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRI5e1g53JKfk3qB8Es0uxuXEoZocGIskYzLIO5WYJ9g&s=10' }}
-style={{
-  width:170,
-  height:120
-}}/>
-</View>
+      source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRI5e1g53JKfk3qB8Es0uxuXEoZocGIskYzLIO5WYJ9g&s=10'
+     }}
+  style={styles.imagem}
+        />
 
-  <Text> Insira seu nome: </Text>
-  <TextInput
-  placeholder='seu nome'
-  style={{
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 5
+      </View>
+
+      <Text>
+        Insira seu nome:
+      </Text>
+
+   <TextInput
+   placeholder="seu nome"
+   style={styles.input}
+      />
+
+      <View>
+
+        <Text>
+          Insira sua senha:
+     </Text>
+
+    <TextInput
+     placeholder="sua senha"
+      keyboardType="numeric"
+     secureTextEntry={true}
+  style={styles.input}
+        />
+
+ <Button
+    title="enviar"
+    onPress={() => {
+    alert("enviado");
+    }}
+    disabled={false}
+ />
+
+    <Pressable
+ onPress={() => {
+  alert("criar nova senha");
   }}
-  />
- <View>
-  <Text> Insira sua senha: </Text>
-  <TextInput
-  placeholder='sua senha'
-  keyboardType='numeric'
-  secureTextEntry={true}
-  style={{
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 5
-  }}
-  />
+    >
+   <Text>
+ esqueci minha senha:
+  </Text>
+  </Pressable>
 
-<Button
-title='enviar'
-onPress={ () => {alert("enviado") }} 
-disabled={ false }
-/>
+ </View>
 
-
-<Pressable
-onPress={ () => {alert("criar nova senha") }}>
-<Text> esqueci minha senha: </Text>
-</Pressable>
-
-</View>
-  </View>
-
-);
+ </View>
+  );
 }
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+  },
+
+  imagemContainer: {
+    flex: 64,
+    alignItems: 'center',
+  },
+  imagem: {
+    width: 170,
+    height: 120,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+  },
+
+});
